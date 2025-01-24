6a7b226b9851734f637a28c362ad217f53f6c3af
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2025-01-24 12:03:54 +0100

    Change from 2 floats to AbsoluteCoords3D

--- core/src/main/java/fr/bloomyindev/cgj2024/Star.java
+++ core/src/main/java/fr/bloomyindev/cgj2024/Star.java
@@ -4,12 +4,12 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import fr.bloomyindev.cgj2024.CoordinateSystems.AbsoluteCoords3D;
 
 public class Star {
     private Sound sound;
     private Color color;
-    private float x;
-    private float y;
+    private AbsoluteCoords3D coords;
 
     public Sound getSound() {
         return sound;
@@ -19,8 +19,8 @@ public class Star {
         return color;
     }
 
-    public float[] getCoordinates() {
-        return new float[]{x, y};
+    public AbsoluteCoords3D getCoordinates() {
+        return coords;
     }
 
     public void render(ShapeRenderer shapeRenderer, int x, int y, int distance) {
