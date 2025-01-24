002389b1aaec73398af61f579bdf4c8b3c46d0d7
Clement Dubois <clement.dubois02@etu.umontpellier.fr>
2025-01-24 14:03:15 +0100

    create blackhole and define if the spaceship is inside to blackhole

---
+++ core/src/main/java/fr/bloomyindev/cgj2024/BlackHole.java
@@ -0,0 +1,20 @@
+package fr.bloomyindev.cgj2024;
+
+import fr.bloomyindev.cgj2024.CoordinateSystems.AbsoluteCoords3D;
+import fr.bloomyindev.cgj2024.CoordinateSystems.SpaceshipRelative;
+
+public class BlackHole {
+    private float height;
+    private AbsoluteCoords3D coords3d;
+
+    public BlackHole(float height, AbsoluteCoords3D coords3d) {
+        this.height = height;
+        this.coords3d = coords3d;
+    }
+    
+    public boolean spaceshipInsideBlackHole(Spaceship spaceship) {
+        SpaceshipRelative space = new SpaceshipRelative(this.coords3d,spaceship.getSpaceshipCord());
+        return space.getDistance() <= height;
+    }
+
+}
