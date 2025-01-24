15265fa27d7cbe5a4f6de241f257f4f26294a9cf
Clement Dubois <clement.dubois02@etu.umontpellier.fr>
2025-01-24 10:20:21 +0100

    ship creation added

---
+++ core/src/main/java/fr/bloomyindev/cgj2024/Spaceship.java
@@ -0,0 +1,16 @@
+package fr.bloomyindev.cgj2024;
+
+public class Spaceship {
+    private float position;
+    private float latitude;
+    private float longitude;
+    private int distance;
+
+    public Spaceship(float position, float latitude, float longitude, int distance) {
+        this.position = position;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.distance = 0;
+    }
+}
+
