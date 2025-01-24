42bd85f30aa0e6584125d668ad959789d76e334c
Clement Dubois <clement.dubois02@etu.umontpellier.fr>
2025-01-24 10:53:50 +0100

    add manage of longitude and latitude

--- core/src/main/java/fr/bloomyindev/cgj2024/Spaceship.java
+++ core/src/main/java/fr/bloomyindev/cgj2024/Spaceship.java
@@ -1,16 +1,42 @@
 package fr.bloomyindev.cgj2024;
 
 public class Spaceship {
-    private float position;
     private float latitude;
     private float longitude;
     private int distance;
 
-    public Spaceship(float position, float latitude, float longitude, int distance) {
-        this.position = position;
+    public Spaceship(float latitude, float longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.distance = 0;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void rotateLongitude(float longitude) {
+        if (this.longitude < longitude) {
+            for (float i = this.longitude; i < longitude; i++) {
+                this.longitude += i;
+            }
+        } else {
+            for (float i = this.longitude; i > longitude; i--) {
+                this.longitude += i;
+            }
+        }
+    }
+
+    public void rotateLatitude(float latitude) {
+        if (this.latitude < latitude) {
+            for (float i = this.latitude; i < latitude; i++) {
+                this.latitude += i;
+            }
+        } else {
+            for (float i = this.latitude; i > latitude; i--) {
+                this.latitude += i;
+            }
+        }
     }
 }
 
