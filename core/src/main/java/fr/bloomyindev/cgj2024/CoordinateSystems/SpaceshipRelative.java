26ba167133fa948e0176d2d60c25beb226481aa7
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-24 11:02:44 +0100

    Ajout de l'enregistrement des coordonnées absolues de l'objet dans SpaceshipRelative

--- core/src/main/java/fr/bloomyindev/cgj2024/CoordinateSystems/SpaceshipRelative.java
+++ core/src/main/java/fr/bloomyindev/cgj2024/CoordinateSystems/SpaceshipRelative.java
@@ -3,11 +3,13 @@ package fr.bloomyindev.cgj2024.CoordinateSystems;
 import fr.bloomyindev.cgj2024.Ut;
 
 public class SpaceshipRelative {
+	private AbsoluteCoords3D coords3d;
 	private float lat, lng;
 	private float xRel, yRel, zRel;
 	private long distance;
 
 	public SpaceshipRelative(AbsoluteCoords3D coords3d, AbsoluteCoords3D spaceshipCoords3d) {
+		this.coords3d = coords3d;
 		float[] Delta3D = spaceshipCoords3d.getDelta(coords3d);
 
 		this.xRel = Delta3D[0];
