e39b403a251e3e46d02848e064fa05e32e1b821e
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-24 14:06:43 +0100

    Getter pour lat et lng du centre du fov

--- core/src/main/java/fr/bloomyindev/cgj2024/CoordinateSystems/FieldOfViewCoords.java
+++ core/src/main/java/fr/bloomyindev/cgj2024/CoordinateSystems/FieldOfViewCoords.java
@@ -1,5 +1,19 @@
 package fr.bloomyindev.cgj2024.CoordinateSystems;
 
 public class FieldOfViewCoords {
-	
+	private float normalisedX, normalisedY;
+
+	private FieldOfView FOV;
+
+	public FieldOfViewCoords(FieldOfView FOV, SpaceshipRelative objRelativeCoords) {
+		this.FOV = FOV;
+	}
+
+	public void reComputeFOVCoords(SpaceshipRelative objRelativeCoords) {
+		float DeltaLat, DeltaLng;
+
+		float[] objCoords = objRelativeCoords.getLatLong();
+		DeltaLat = objCoords[0] - FOV.;
+		DeltaLng = objCoords[1];
+	}
 }
