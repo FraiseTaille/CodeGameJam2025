1e7e1d76d6a9e092ed80e5244cff28ef16eb53e4
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-24 11:18:30 +0100

    Ajout d'une méthode pour recalculer les coordonnées relatives et adaptation du constructeur

--- core/src/main/java/fr/bloomyindev/cgj2024/CoordinateSystems/SpaceshipRelative.java
+++ core/src/main/java/fr/bloomyindev/cgj2024/CoordinateSystems/SpaceshipRelative.java
@@ -10,16 +10,7 @@ public class SpaceshipRelative {
 
 	public SpaceshipRelative(AbsoluteCoords3D coords3d, AbsoluteCoords3D spaceshipCoords3d) {
 		this.coords3d = coords3d;
-		float[] Delta3D = spaceshipCoords3d.getDelta(coords3d);
-
-		this.xRel = Delta3D[0];
-		this.yRel = Delta3D[1];
-		this.zRel = Delta3D[2];
-
-		this.distance = (int)Math.sqrt(Ut.pow(xRel, 2.0f) + Ut.pow(yRel, 2.0f) + Ut.pow(zRel, 2.0f)); // Distance de l'étoile au vaisseau
-
-		this.lat = (long)Math.asin(zRel / (float)distance);
-		this.lng = (long)Math.atan2(xRel / (float)distance, yRel / (float)distance);
+		this.reComputeRelativeCoords(spaceshipCoords3d);
 	}
 
 	public long getDistance() {
@@ -32,5 +23,18 @@ public class SpaceshipRelative {
 
 	public float[] getLatLong() {
 		return new float[]{this.lat, this.lng};
+	}
+
+	public void reComputeRelativeCoords(AbsoluteCoords3D spaceshipCoords3d) {
+		float[] Delta3D = spaceshipCoords3d.getDelta(this.coords3d);
+
+		this.xRel = Delta3D[0];
+		this.yRel = Delta3D[1];
+		this.zRel = Delta3D[2];
+
+		this.distance = (int)Math.sqrt(Ut.pow(xRel, 2.0f) + Ut.pow(yRel, 2.0f) + Ut.pow(zRel, 2.0f)); // Distance de l'étoile au vaisseau
+
+		this.lat = (long)Math.asin(zRel / (float)distance);
+		this.lng = (long)Math.atan2(xRel / (float)distance, yRel / (float)distance);
 	}
 }
