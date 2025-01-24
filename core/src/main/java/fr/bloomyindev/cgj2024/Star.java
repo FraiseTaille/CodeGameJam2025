76923dc272a6259c79360c1e2e64c716140a73b9
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-24 15:53:41 +0100

    Ajout de la gestion de la taille angulaire et mise à jour de la méthode render pour prendre en compte l'angle de FOV

--- core/src/main/java/fr/bloomyindev/cgj2024/Star.java
+++ core/src/main/java/fr/bloomyindev/cgj2024/Star.java
@@ -4,12 +4,15 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import fr.bloomyindev.cgj2024.CoordinateSystems.AbsoluteCoords3D;
+import fr.bloomyindev.cgj2024.CoordinateSystems.*;
 
 public class Star {
     private Sound sound;
     private Color color;
     private AbsoluteCoords3D coords;
+
+    private float angularSize;
+    private int absoluteRadius;
 
     public Sound getSound() {
         return sound;
@@ -23,8 +26,17 @@ public class Star {
         return coords;
     }
 
-    public void render(ShapeRenderer shapeRenderer, int x, int y, int distance) {
+    public void render(ShapeRenderer shapeRenderer, int x, int y, int distance, float fovAngleX) {
+        float renderSize;
+
+        renderSize = angularSize * (16 / fovAngleX);
+
         shapeRenderer.setColor(color);
-        shapeRenderer.circle(x, y, 10);
+        shapeRenderer.circle(x, y, renderSize);
+    }
+
+    public void computeAngularSize(long distance, float fovAngleX) {
+        angularSize = 2 * (float) Math.asin((float) (2 * this.absoluteRadius / distance));
+        angularSize /= fovAngleX; // Pour convertir en format normalisé
     }
 }
